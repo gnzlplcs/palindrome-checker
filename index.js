7bfab0af8c61ba6aaee9820e304c7e729a009f72
gnzlplcs <palaciosgm@outlook.com>
2022-04-03 20:56:52 -0500

    :pencil2: fix typo

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function palindrome(str) {
+const palindrome = (str) => {
   let cleanWord = cleanStr(str);
-  let reverseWord = '';
+  let reverseWord = "";
   for (let i = cleanWord.length - 1; i >= 0; i--) {
-    reverseWord += cleanWord[i]
+    reverseWord += cleanWord[i];
   }
   return cleanWord === reverseWord ? true : false;
 }
 
-const cleanStr = str => {
-  let regexValidator = /[a-z0-9]/ig;
-  let cleanStr = str.match(regexValidator)
-  let output = cleanStr.join('').toLowerCase();
+const cleanStr = (str) => {
+  let regexValidator = /[a-z0-9]/gi;
+  let cleanStr = str.match(regexValidator);
+  let output = cleanStr.join("").toLowerCase();
   return output;
-}
+};
 
 console.log(palindrome("eye")); // true
 console.log(palindrome("_eye")); // true
@@ -24,5 +24,5 @@ console.log(palindrome("nope")); // false
 console.log(palindrome("almostomla")); // false
 console.log(palindrome("My age is 0, 0 si ega ym.")); // true
 console.log(palindrome("1 eye for of 1 eye.")); // false
-console.log(palindrome("0_0 (: /-\ :) 0-0")); // true
-console.log(palindrome("five|\_/|four")); // false
+console.log(palindrome("0_0 (: /- :) 0-0")); // true
+console.log(palindrome("five|_/|four")); // false
